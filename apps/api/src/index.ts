26986ac8ea25018fb8b7a1315cf5aeda7a741b1a
Stiven Felipe <95578488+StivenF0@users.noreply.github.com>
2026-03-24 10:55:19 -0300

    feat: setup elysia server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,17 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+import { Elysia } from "elysia";
+import cors from '@elysiajs/cors';
 
 const client = createClient({ url: process.env.DB_FILE_NAME! });
 const db = drizzle({ client });
+
+const app = new Elysia()
+  .use(cors())
+	.get('/', () => 'Hello Elysia')
+	.listen(8080)
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+)
