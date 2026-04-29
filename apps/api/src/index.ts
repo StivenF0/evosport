204cf1378b3ec25fec35610dafc65639b0180ffb
Stiven Felipe <95578488+StivenF0@users.noreply.github.com>
2026-04-29 10:20:02 -0300

    feat: modify index.ts to run the new routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,17 +1,23 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
 import { Elysia } from "elysia";
 import cors from '@elysiajs/cors';
 
-const client = createClient({ url: process.env.DB_FILE_NAME! });
-const db = drizzle({ client });
+import { eventRoutes } from './routes/event-routes.ts';
+import { venueRoutes } from './routes/venue-routes.ts';
+import { matchRoutes } from './routes/match-routes.ts';
+import { teamRoutes } from './routes/team-routes.ts';
+import { rankingRoutes } from './routes/ranking-routes.ts';
 
-const app = new Elysia()
+const app = new Elysia({ prefix: '/api'})
   .use(cors())
-	.get('/', () => 'Hello Elysia')
-	.listen(8080)
+  .get('/', () => 'API Evosport rodando com sucesso!')
+  .use(eventRoutes)
+  .use(venueRoutes)
+  .use(matchRoutes)
+  .use(teamRoutes)
+  .use(rankingRoutes)
+  .listen(8080);
 
 console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-)
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
